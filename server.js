46b6c0a7ade8350718ca3568b3f366be831e41c5
aKingsView <106622740+aKingsView@users.noreply.github.com>
2022-12-29 14:48:20 -0500

    updated db.json

---
+++ server.js
@@ -0,0 +1,81 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const fs = require("fs");
+const db = require("./db/db.json");
+const path = require("path");
+const notes = require("./db/db.json");
+
+app.use(bodyParser.json());
+
+//it uses JSON formatting
+app.use(express.json());
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+
+// Add a static middleware for serving assets in the public folder
+app.use(express.static("public"));
+// app.use(express.static(path.join(__dirname,"./public")));
+
+app.listen(3000, () => {
+    console.log('Server is listening on port 3000');
+  });
+
+  
+  app.get('/api/notes', (req, res) => {
+    // read the notes from the JSON file
+    fs.readFile('db/db.json', (err, data) => {
+      if (err) throw err;
+        const notes = JSON.parse(data);
+        res.send(notes);
+    });
+  });
+  
+  app.get('/api/notes/:id', (req, res) => {
+    // read the notes from the JSON file
+    fs.readFile('db/db.json', (err, data) => {
+      if (err) throw err;
+        const notes = JSON.parse(data);
+        res.send(notes);
+    });
+  });
+  
+
+  app.post('/api/notes', (req, res) => {
+    fs.readFile('db/db.json', (err, data) => {
+      if (err) throw err;
+  
+      const notes = JSON.parse(data);
+      const newNote = req.body;
+  
+     notes.push(newNote);
+      fs.writeFile('db/db.json', JSON.stringify(notes), (err) => {
+        if (err) throw err;
+        console.log('Note added successfully');
+      });
+        res.send('Note added successfully');
+    });
+  });
+  
+
+  app.delete('/api/notes/:id', (req, res) => {
+    // read the notes from the JSON file
+    console.log("id= " +req.params.id);
+    fs.readFile('db/db.json', (err, data) => {
+      if (err) throw err;
+  
+      const notes = JSON.parse(data);
+      const index = notes.findIndex((note) => note.id === req.params.id);
+  
+      notes.splice(index, 1);
+  
+      fs.writeFile('db/db.json', JSON.stringify(notes), (err) => {
+        if (err) throw err;
+        console.log('Note deleted successfully');
+      });
+  
+      res.send('Note deleted successfully');
+    });
+  });
+  
+    
